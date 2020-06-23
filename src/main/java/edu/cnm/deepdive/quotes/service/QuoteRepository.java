9ce3cd58125f92c4e65987f352f36665de205348
Anastasia Hristian <an.hristian@gmail.com>
2020-06-23 11:10:03 -0600

    Controller method for individual resources.

--- src/main/java/edu/cnm/deepdive/quotes/service/QuoteRepository.java
+++ src/main/java/edu/cnm/deepdive/quotes/service/QuoteRepository.java
@@ -1,11 +1,14 @@
 package edu.cnm.deepdive.quotes.service;
 
 import edu.cnm.deepdive.quotes.model.entity.Quote;
+import edu.cnm.deepdive.quotes.model.entity.Source;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
 
   Iterable<Quote> getAllByOrderByTextAsc();
 
+//this methods will query the quotes by source.Return all quotes by the chosen source.
+  Iterable<Quote> getAllBySourceOrderByTextAsc(Source source);
 
 }
