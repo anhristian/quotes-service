1f78ff46649326a3d25b18fc808ebd7853d7a940
Anastasia Hristian <an.hristian@gmail.com>
2020-07-13 11:23:33 -0600

    Continuing OAuth 2.0 integration.

--- src/main/java/edu/cnm/deepdive/quotes/service/QuoteRepository.java
+++ src/main/java/edu/cnm/deepdive/quotes/service/QuoteRepository.java
@@ -4,7 +4,9 @@ import edu.cnm.deepdive.quotes.model.entity.Quote;
 import edu.cnm.deepdive.quotes.model.entity.Source;
 import edu.cnm.deepdive.quotes.model.entity.Tag;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
 
@@ -18,4 +20,10 @@ public interface QuoteRepository extends JpaRepository<Quote, Long> {
   Iterable<Quote> getAllByTextContainingOrderByTextAsc(String filter);
 
 
+  @Query(value = "SELECT * FROM sa.Quote ORDER BY RANDOM() OFFSET 0 ROWS FETCH NEXT 1 ROW ONLY",
+      nativeQuery = true)
+  Optional<Quote> getRandom();
+
+  @Query("SELECT COUNT(q) FROM Quote AS q")  //q is for quote here
+  long getCount();
 }
